9cf360b3393ca12cf280f06d0f30cf7d2467c17f
unknown <frederikhta@gmail.com>
2024-03-21 22:47:15 +0100

    remove c# debugger and lsp config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 local config = function()
+	require 'nvim-treesitter.install'.prefer_git = false
 	require('nvim-treesitter.configs').setup {
 		-- Add languages to be installed here that you want installed for treesitter
 		ensure_installed = {
